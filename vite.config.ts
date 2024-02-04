c67db184d6866ff0b12b45379269494f7fc94984
Laura deVere-Hopkins <lauradevh@gmail.com>
2024-02-04 16:50:15 -0500

    updated vite config to point to build folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		outDir: "./build",
+	},
 	server: {
 		proxy: {
 			"/api": "http://localhost:5050",
